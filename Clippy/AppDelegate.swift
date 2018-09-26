04cc7e77243c06813e938002f66d0e8a65e38258
hpat0003 <hpat0003@student.monash.edu>
2018-09-26 22:46:35 +1000

    hello, world! 👋🏻

---
+++ Clippy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Clippy
+//
+//  Created by Harsil Patel on 26/9/18.
+//  Copyright © 2018 Harsil Patel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Clippy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Clippy
+//
+//  Created by Harsil Patel on 26/9/18.
+//  Copyright © 2018 Harsil Patel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
