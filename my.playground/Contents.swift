45813c651143b59721697fcc54462880947bf4fd
hpat0003 <hpat0003@student.monash.edu>
2018-09-30 19:31:51 +1000

    virtualKey()

--- my.playground/Contents.swift
+++ my.playground/Contents.swift
@@ -1,23 +1,23 @@
 import Cocoa
 
-let pasteboard = NSPasteboard.general
-
-pasteboard.string(forType: NSPasteboard.PasteboardType.string)
-
-
-var history: [String] = []
-
-func copyToClipboard(string: String) {
-    pasteboard.clearContents()
-    pasteboard.setString(string, forType: NSPasteboard.PasteboardType.string)
-    history.append(string)
-}
-copyToClipboard(string: "hello")
-
-var x: [String] = []
-if x.count > 0
-    {x.removeFirst()}
-x
+//let pasteboard = NSPasteboard.general
+//
+//pasteboard.string(forType: NSPasteboard.PasteboardType.string)
+//
+//
+//var history: [String] = []
+//
+//func copyToClipboard(string: String) {
+//    pasteboard.clearContents()
+//    pasteboard.setString(string, forType: NSPasteboard.PasteboardType.string)
+//    history.append(string)
+//}
+//copyToClipboard(string: "hello")
+//
+//var x: [String] = []
+//if x.count > 0
+//    {x.removeFirst()}
+//x
 
 //pasteboard.pasteboardItems?[0].
 
@@ -28,6 +28,20 @@ x
 //    }
 //}
 //clipboardItems.count
+
+
+
+
+func virtualKey(_ keyCode: CGKeyCode, keyPress: Bool) {
+    let source = CGEventSource(stateID: .combinedSessionState)
+    CGEvent(keyboardEventSource: source,virtualKey: keyCode,keyDown: keyPress)?.post(tap: .cghidEventTap)
+}
+let shiftKey: CGKeyCode = 56
+let z: CGKeyCode = 6
+virtualKey(shiftKey, keyPress: true)
+virtualKey(z, keyPress: true)
+virtualKey(z, keyPress: false)
+virtualKey(shiftKey, keyPress: false)
 
 
 
